9b88f9e715db924cf0fa624481a26e4ee4965977
Benedykt Bela <benedykt.bela@intel.com>
2026-03-12 16:05:55 +0000

    Change common_methods_invocations to make index_reduce deterministic.
    Signed-off-by: Benedykt Bela <benedykt.bela@intel.com>

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -5381,29 +5381,49 @@ def sample_inputs_index_reduce(op_info, device, dtype, requires_grad, **kwargs):
         self_shape, src_shape = shape
         # dim. We handle the scalar case
         dim = 1 if len(self_shape) >= 2 else 0
-        idx = make_idx(src_shape[dim] if len(src_shape) != 0 else 1,
-                       self_shape[dim] if len(self_shape) != 0 else 1)
-        args = (dim, idx, make_arg(src_shape), reduce)
-        yield SampleInput(make_arg(self_shape),
-                          args=args,
-                          kwargs={'include_self' : include_self})
+        
+        # Create XPU tensors
+        idx_xpu = make_idx(src_shape[dim] if len(src_shape) != 0 else 1,
+                           self_shape[dim] if len(self_shape) != 0 else 1)
+        input_xpu = make_arg(self_shape)
+        src_xpu = make_arg(src_shape)
+        
+        # Create identical CPU tensors
+        idx_cpu = idx_xpu.cpu().clone()
+        input_cpu = input_xpu.cpu().clone()
+        src_cpu = src_xpu.cpu().clone()
+        
+        # Restore requires_grad for CPU tensors
+        if requires_grad:
+            input_cpu.requires_grad_(True)
+            src_cpu.requires_grad_(True)
+        
+        # Create XPU sample
+        args_xpu = (dim, idx_xpu, src_xpu, reduce)
+        xpu_sample = SampleInput(input_xpu, args=args_xpu, kwargs={'include_self': include_self})
+        
+        # Create CPU sample with identical values
+        args_cpu = (dim, idx_cpu, src_cpu, reduce)
+        cpu_sample = SampleInput(input_cpu, args=args_cpu, kwargs={'include_self': include_self})
+        
+        yield (xpu_sample, cpu_sample)
 
-    # Sample inputs to test edge cases for backward
-    if requires_grad and reduce == 'prod':
-        # Check that gradients are propagated correctly for prod when zeros in self/src are reduced
-        # This sample tests gradients for the following cases
-        # (a) 1 zero reduced (from source (self[0, 1]), from self (self[0, 0]))
-        # (b) 2 zeros reduced (1 from src and 1 from self (self[1, 0], self[1, 1])
-        # (c) no zeros reduced (self[2, 1], self[2, 2])
-        # (d) 2 zeros reduced (both from src) is tested in test/test_autograd.py
-        #     test_scatter_index_reduce_prod_gradgrad_error as this case is not supported for gradgrad
-        input = torch.tensor([[0, 13], [0, 0], [15, 19]], dtype=dtype, device=device, requires_grad=requires_grad)
-        src = torch.tensor([[2, 0], [0, 0], [2, 3], [2, 2]], dtype=dtype, device=device, requires_grad=requires_grad)
-        idx = torch.tensor([0, 1, 2, 0], dtype=torch.long, device=device)
+    # # Sample inputs to test edge cases for backward
+    # if requires_grad and reduce == 'prod':
+    #     # Check that gradients are propagated correctly for prod when zeros in self/src are reduced
+    #     # This sample tests gradients for the following cases
+    #     # (a) 1 zero reduced (from source (self[0, 1]), from self (self[0, 0]))
+    #     # (b) 2 zeros reduced (1 from src and 1 from self (self[1, 0], self[1, 1])
+    #     # (c) no zeros reduced (self[2, 1], self[2, 2])
+    #     # (d) 2 zeros reduced (both from src) is tested in test/test_autograd.py
+    #     #     test_scatter_index_reduce_prod_gradgrad_error as this case is not supported for gradgrad
+    #     input = torch.tensor([[0, 13], [0, 0], [15, 19]], dtype=dtype, device=device, requires_grad=requires_grad)
+    #     src = torch.tensor([[2, 0], [0, 0], [2, 3], [2, 2]], dtype=dtype, device=device, requires_grad=requires_grad)
+    #     idx = torch.tensor([0, 1, 2, 0], dtype=torch.long, device=device)
 
-        yield SampleInput(input,
-                          args=(0, idx, src, reduce),
-                          kwargs={'include_self': True})
+    #     yield SampleInput(input,
+    #                       args=(0, idx, src, reduce),
+    #                       kwargs={'include_self': True})
 
 def sample_inputs__unsafe_masked_index(op_info, device, dtype, requires_grad, **kwargs):
     make_arg = partial(make_tensor, device=device, dtype=dtype, requires_grad=requires_grad)
